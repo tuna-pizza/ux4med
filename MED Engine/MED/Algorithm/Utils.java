29d83cc0c58ee89c357653905debf3a1261719c8
Julia Katheder <julia@katheder.com>
2023-04-28 12:10:19 +0200

    Add morphType EASING

--- MED Engine/MED/Algorithm/Utils.java
+++ MED Engine/MED/Algorithm/Utils.java
@@ -5,7 +5,6 @@ import MED.Data.Coordinate;
 import MED.Engine.MEDEngine;
 import MED.Graph.MEDAnimation;
 import MED.Graph.MEDEdge;
-import com.yworks.yfiles.utils.ICollection;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.CubicCurve;
@@ -15,48 +14,46 @@ import javafx.scene.shape.Shape;
 import java.util.LinkedList;
 import java.util.List;
 
-public final class Utils
-{
+public final class Utils {
     static Utils utils;
     private MEDEngine engine;
-    private Utils()
-    {
+
+    private Utils() {
         this.engine = new MEDEngine();
     }
-    public MEDEngine getEngine()
-    {
+
+    public MEDEngine getEngine() {
         return engine;
     }
-    private static void init()
-    {
-        if (utils == null)
-        {
+
+    private static void init() {
+        if (utils == null) {
             utils = new Utils();
         }
     }
-    public static double lineAt(double a, double b, double x)
-    {
-        return a*x +b;
+
+    public static double lineAt(double a, double b, double x) {
+        return a * x + b;
     }
-    public static double distance(double x1, double y1, double x2, double y2)
-    {
-        return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
-    public static double distance(Coordinate p, Coordinate q)
-    {
-        return distance(p.getX(),p.getY(),q.getX(),q.getY());
+
+    public static double distance(Coordinate p, Coordinate q) {
+        return distance(p.getX(), p.getY(), q.getX(), q.getY());
     }
-    public static double getMorphingRatio(MEDEdge e, Coordinate p)
-    {
+
+    public static double getMorphingRatio(MEDEdge e, Coordinate p) {
         init();
         double stubMorphingLength = getStubMorphLength(e);
         Coordinate start1 = utils.getEngine().computeStartPosition1(e);
-        Coordinate start2 =  utils.getEngine().computeStartPosition2(e);
-        double distance = Math.min(distance(start1,p),distance(start2,p));
-        return distance/stubMorphingLength;
+        Coordinate start2 = utils.getEngine().computeStartPosition2(e);
+        double distance = Math.min(distance(start1, p), distance(start2, p));
+        return distance / stubMorphingLength;
     }
-    public static double getStubMorphLength(MEDEdge e)
-    {
+
+    public static double getStubMorphLength(MEDEdge e) {
         init();
         Coordinate mid = utils.getEngine().computeMidPosition(e);
         Coordinate start = utils.getEngine().computeStartPosition1(e);
@@ -67,8 +64,7 @@ public final class Utils
         return distance(midX, midY, startX, startY);
     }
 
-    public static Coordinate computeCrossing(MEDEdge e, MEDEdge c)
-    {
+    public static Coordinate computeCrossing(MEDEdge e, MEDEdge c) {
         init();
         Coordinate startE1 = utils.getEngine().computeStartPosition1(e);
         Coordinate startE2 = utils.getEngine().computeStartPosition2(e);
@@ -76,16 +72,13 @@ public final class Utils
         double rightE = Math.max(startE1.getX(), startE2.getX());
         double bottomE = Math.min(startE1.getY(), startE2.getY());
         double topE = Math.max(startE1.getY(), startE2.getY());
-        double aE=0;
-        double bE=0;
+        double aE = 0;
+        double bE = 0;
         boolean verticalE = false;
-        if (leftE != rightE)
-        {
-            aE = (startE1.getY()-startE2.getY())/(startE1.getX()-startE2.getX());
+        if (leftE != rightE) {
+            aE = (startE1.getY() - startE2.getY()) / (startE1.getX() - startE2.getX());
             bE = startE1.getY() - aE * startE1.getX();
-        }
-        else
-        {
+        } else {
             verticalE = true;
         }
         Coordinate startC1 = utils.getEngine().computeStartPosition1(c);
@@ -97,21 +90,16 @@ public final class Utils
         double aC = 0;
         double bC = 0;
         boolean verticalC = false;
-        if (leftC != rightC)
-        {
-            aC = (startC1.getY()-startC2.getY())/(startC1.getX()-startC2.getX());
+        if (leftC != rightC) {
+            aC = (startC1.getY() - startC2.getY()) / (startC1.getX() - startC2.getX());
             bC = startC1.getY() - aC * startC1.getX();
-        }
-        else
-        {
+        } else {
             verticalC = true;
         }
-        if (verticalC && verticalE)
-        {
+        if (verticalC && verticalE) {
             return null;
         }
-        if (verticalC)
-        {
+        if (verticalC) {
             double leftSwap = leftC;
             double rightSwap = rightC;
             double aSwap = aC;
@@ -119,7 +107,7 @@ public final class Utils
             leftC = leftE;
             rightC = rightE;
             aC = aE;
-            bC= bE;
+            bC = bE;
             leftE = leftSwap;
             rightE = rightSwap;
             bottomE = bottomC;
@@ -128,148 +116,116 @@ public final class Utils
             bE = bSwap;
             verticalE = true;
         }
-        if (verticalE)
-        {
-            if (leftC <= leftE && rightC >= leftE)
-            {
-                double y = lineAt(aC,bC,leftE);
-                if (topE >= y && y >= bottomE)
-                {
-                    return new Coordinate(leftE,y);
+        if (verticalE) {
+            if (leftC <= leftE && rightC >= leftE) {
+                double y = lineAt(aC, bC, leftE);
+                if (topE >= y && y >= bottomE) {
+                    return new Coordinate(leftE, y);
                 }
             }
-        }
-        else
-        {
-            double x = (bC-bE)/(aE-aC);
-            if (leftC <= x && x <= rightC)
-            {
-                if (leftE <= x && x <= rightE)
-                {
-                    double y = lineAt(aC,bC,x);
-                    return new Coordinate(x,y);
+        } else {
+            double x = (bC - bE) / (aE - aC);
+            if (leftC <= x && x <= rightC) {
+                if (leftE <= x && x <= rightE) {
+                    double y = lineAt(aC, bC, x);
+                    return new Coordinate(x, y);
                 }
             }
         }
         return null;
     }
 
-    public static double getTimeForMorph(double morphingRatio, double stubMorphTime, MEDAnimation.MorphType morphType)
-    {
-        switch (morphType)
-        {
-            case LINEAR:
-            {
-                return morphingRatio*stubMorphTime;
+    public static double getTimeForMorph(double morphingRatio, double stubMorphTime, MEDAnimation.MorphType morphType) {
+        switch (morphType) {
+            case LINEAR: {
+                return morphingRatio * stubMorphTime;
             }
-            case SINE:
-            {
-                //morphingRatio = sin(time * pi/2)
-                //2 arcsin(morphingRatio)/pi = time
-                return ((2*Math.asin(morphingRatio))/Math.PI)*stubMorphTime;
+            case SINE: {
+                // morphingRatio = sin(time * pi/2)
+                // 2 arcsin(morphingRatio)/pi = time
+                return ((2 * Math.asin(morphingRatio)) / Math.PI) * stubMorphTime;
             }
-            case INVERSESINE:
-            {
-                //morphingRatio = 1-Math.cos(time*Math.PI/2);
-                return (2*Math.acos(1-morphingRatio)/Math.PI)*stubMorphTime;
+            case INVERSESINE: {
+                // morphingRatio = 1-Math.cos(time*Math.PI/2);
+                return (2 * Math.acos(1 - morphingRatio) / Math.PI) * stubMorphTime;
             }
-            case COSINE:
-            {
-                //morphingRatio = (1-Math.cos(time*Math.PI))/2;
-                //1-2morphingRatio = Math.cos(time*Math.PI)
-                double timeRatio = Math.acos(1-2*morphingRatio);
-                if (timeRatio < 0)
-                {
+            case COSINE: {
+                // morphingRatio = (1-Math.cos(time*Math.PI))/2;
+                // 1-2morphingRatio = Math.cos(time*Math.PI)
+                double timeRatio = Math.acos(1 - 2 * morphingRatio);
+                if (timeRatio < 0) {
                     timeRatio += Math.PI;
                 }
-                return (timeRatio/Math.PI)*stubMorphTime;
+                return (timeRatio / Math.PI) * stubMorphTime;
             }
             case EASING:
-                double timeRatio = Utils.cubicBezierCurveAt(0.1,0.25,1,0.25,morphingRatio);
-                return timeRatio*stubMorphTime;
-            default:
-            {
+                double timeRatio = Utils.cubicBezierCurveAt(0.1, 0.25, 1, 0.25, morphingRatio);
+                return timeRatio * stubMorphTime;
+            default: {
                 return -1;
             }
         }
     }
 
-    public static double earliestSpace(List<Interval> intervals)
-    {
+    public static double earliestSpace(List<Interval> intervals) {
         double t = 0;
         intervals.sort(new IntervalStartComparator());
-        for (int i=0; i < intervals.size(); i++)
-        {
-            if (intervals.get(i).getEnd() < t)
-            {
+        for (int i = 0; i < intervals.size(); i++) {
+            if (intervals.get(i).getEnd() < t) {
                 continue;
-            }
-            else if (t < intervals.get(i).getStart())
-            {
+            } else if (t < intervals.get(i).getStart()) {
                 return t;
-            }
-            else
-            {
+            } else {
                 t = intervals.get(i).getEnd();
             }
         }
         return t;
     }
 
-    public static Interval getConflictInterval(MEDEdge e, MEDEdge c, double startTimeC, double speed, double fullLengthTime, double crossingDelay, MEDAnimation.MorphType morphType)
-    {
+    public static Interval getConflictInterval(MEDEdge e, MEDEdge c, double startTimeC, double speed,
+            double fullLengthTime, double crossingDelay, MEDAnimation.MorphType morphType) {
         double stubMorphLengthE = Utils.getStubMorphLength(e);
-        double stubMorphTimeE = stubMorphLengthE/speed;
-        double fullMorphTimeE = 2*stubMorphTimeE + fullLengthTime;
-        Coordinate crossing = Utils.computeCrossing(e,c);
-        if (crossing != null)
-        {
-            double morphingRatioE = Utils.getMorphingRatio(e,crossing);
-            double crossingMorphTimeE1 = Utils.getTimeForMorph(morphingRatioE,stubMorphTimeE,morphType);
+        double stubMorphTimeE = stubMorphLengthE / speed;
+        double fullMorphTimeE = 2 * stubMorphTimeE + fullLengthTime;
+        Coordinate crossing = Utils.computeCrossing(e, c);
+        if (crossing != null) {
+            double morphingRatioE = Utils.getMorphingRatio(e, crossing);
+            double crossingMorphTimeE1 = Utils.getTimeForMorph(morphingRatioE, stubMorphTimeE, morphType);
             double crossingMorphTimeE2 = fullMorphTimeE - crossingMorphTimeE1;
-            double morphingRatioC = Utils.getMorphingRatio(c,crossing);
+            double morphingRatioC = Utils.getMorphingRatio(c, crossing);
             double stubMorphLengthC = Utils.getStubMorphLength(c);
-            double stubMorphTimeC = stubMorphLengthC/speed;
-            double fullMorphTimeC = 2*stubMorphTimeC + fullLengthTime;
-            double crossingMorphTimeC1 = Utils.getTimeForMorph(morphingRatioC,stubMorphTimeC,morphType);
+            double stubMorphTimeC = stubMorphLengthC / speed;
+            double fullMorphTimeC = 2 * stubMorphTimeC + fullLengthTime;
+            double crossingMorphTimeC1 = Utils.getTimeForMorph(morphingRatioC, stubMorphTimeC, morphType);
             double crossingMorphTimeC2 = fullMorphTimeC - crossingMorphTimeC1;
             double r1 = startTimeC + crossingMorphTimeC1 - crossingMorphTimeE2;
             double r2 = startTimeC + crossingMorphTimeC2 - crossingMorphTimeE1;
-            if (r1 > r2)
-            {
+            if (r1 > r2) {
                 double swap = r1;
                 r1 = r2;
                 r2 = swap;
             }
-            //add some time between stubs passing the crossing
-            r1 = r1-crossingDelay;
-            r2 = r2+crossingDelay;
-            return new Interval(r1,r2);
-        }
-        else
-        {
+            // add some time between stubs passing the crossing
+            r1 = r1 - crossingDelay;
+            r2 = r2 + crossingDelay;
+            return new Interval(r1, r2);
+        } else {
             return null;
         }
     }
 
-    public static LinkedList<MEDEdge> getCrossingFreeEdges(List<MEDEdge> edges)
-    {
+    public static LinkedList<MEDEdge> getCrossingFreeEdges(List<MEDEdge> edges) {
         LinkedList<MEDEdge> crossingFreeEdges = new LinkedList<>();
-        for (MEDEdge e1 : edges)
-        {
+        for (MEDEdge e1 : edges) {
             boolean isCrossing = false;
-            for (MEDEdge e2 : edges)
-            {
-                if (!e1.equals(e2))
-                {
-                    if (Utils.computeCrossing(e1, e2) != null)
-                    {
+            for (MEDEdge e2 : edges) {
+                if (!e1.equals(e2)) {
+                    if (Utils.computeCrossing(e1, e2) != null) {
                         isCrossing = true;
                     }
                 }
             }
-            if (!isCrossing)
-            {
+            if (!isCrossing) {
                 crossingFreeEdges.add(e1);
             }
         }
@@ -277,11 +233,66 @@ public final class Utils
     }
 
     /**
-     * Computes the y-value of the cubic Bezier curve from (0,0) to (1,1) via control points (x1,y1) and (x2,y2) at x-coordinate x
+     * Computes the y-value of the cubic Bezier curve from (0,0) to (1,1) via
+     * control points (x1,y1) and (x2,y2) at x-coordinate x
      */
-    static public double cubicBezierCurveAt(double x1,double y1,double x2,double y2,double x)
-    {
-        //TODO: implement this function
-        return 0.5;
+    public static double cubicBezierCurveAt(double x1, double y1, double x2, double y2, double x) {
+
+        double epsilon = 0.0000001d;
+        double cx = 3.0 * x1;
+        double bx = 3.0 * (x2 - x1) - cx;
+        double ax = 1.0 - cx - bx;
+        double cy = 3.0 * y1;
+        double by = 3.0 * (y2 - y1) - cy;
+        double ay = 1.0 - cy - by;
+
+        if (x1 == y1 && x2 == y2)
+            return x;
+        if (Math.abs(1.0 - x) < epsilon)
+            return 1.0;
+        if (Math.abs(0.0 - x) < epsilon)
+            return 0.0;
+
+        double t = approximateT(ax, bx, cx, ay, by, cy, x, epsilon);
+        return ((ay * t + by) * t + cy) * t;
     }
+
+    private static double approximateT(double ax, double bx, double cx, double ay, double by, double cy, double x,
+            double epsilon) {
+
+        // Newton method
+        int iterations = 8;
+        double t = x;
+
+        for (int i = 0; i < iterations; i++) {
+            double x2 = ((ax * t + bx) * t + cx) * t;
+            if (Math.abs(x2 - x) < epsilon) {
+                return t;
+            }
+            double dx = (3.0 * ax * t + 2.0 * bx) * t + cx;
+            if (dx < epsilon) {
+                break;
+            }
+            t -= (x2 - x) / dx;
+        }
+
+        // Bisection method
+        double lo = 0.0, hi = 1.0;
+        t = x;
+        while (lo < hi) {
+            double x2 = ((ax * t + bx) * t + cx) * t;
+            if (Math.abs(x2 - x) < epsilon) {
+                return t;
+            }
+            if (x > x2) {
+                lo = t;
+            } else {
+                hi = t;
+            }
+            t = (hi - lo) / 2.0 + lo;
+        }
+        return t;
+
+    }
+
 }
